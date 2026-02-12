184995ff31692cafcec2d5aac9f931f8c5a59463
mustapha <mustaphaamoutaki@gmail.com>
2026-02-12 19:41:51 +0100

    feat: complete edit profile data

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    publicApi:"https://www.arbeitnow.com/api/job-board-api", // keep this as challenge No search 
+    publicApi:"https://www.arbeitnow.com/api/job-board-api", // keep this as challenge No search endpoint no id with response 
+    // publicApi:"https://www.themuse.com/api/public/jobs?page=0&api_key=14ff4bc78441dc714cfd90029a9f776069097fd43d855a19857df3bf6e3e8df4",
     localApi: 'http://localhost:3000'
 };
