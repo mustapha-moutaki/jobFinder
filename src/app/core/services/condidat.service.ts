b014323c82c28f633443ef13301c553a2bb0f56c
mustapha <mustaphaamoutaki@gmail.com>
2026-02-17 23:36:10 +0000

    style: organze the code

--- src/app/core/services/condidat.service.ts
+++ src/app/core/services/condidat.service.ts
@@ -1,29 +1,33 @@
-import { inject, Injectable } from "@angular/core";
-import { CondidatApi } from "../../api/condidat.api";
-import { Observable } from "rxjs";
-import { Condidat } from "../models/condidat.model";
+import { inject, Injectable } from '@angular/core';
+import { CondidatApi } from '../../api/condidat.api';
+import { Observable } from 'rxjs';
+import { Condidat } from '../models/condidat.model';
 
+@Injectable({ providedIn: 'root' })
+export class CondidatService {
+  private readonly condidatApi = inject(CondidatApi);
 
-@Injectable({providedIn: 'root'})
-export class CondidatService{
-    private readonly condidatApi = inject(CondidatApi);
+  // add to
+  addToCondidat(
+    userId: number,
+    jobSlug: string,
+    title: string,
+    company: string,
+    location: string,
+    url: string,
+  ) {
+    return this.condidatApi.addToCondidat(userId, jobSlug, title, company, location, url);
+  }
 
-      // add to
-    addToCondidat(userId: number,  jobSlug: string, title: string, company: string, location: string, url:string){
-      return this.condidatApi.addToCondidat(userId, jobSlug, title, company, location, url);
-    }
+  getCondidatsByUserId(id: number) {
+    return this.condidatApi.getCondidatsByUserId(id);
+  }
 
+  updateCondidatStatus(id: number, status: string) {
+    return this.condidatApi.updateCondidatStatus(id, status);
+  }
 
-    getCondidatsByUserId(id: number){
-        return this.condidatApi.getCondidatsByUserId(id);
-    }
-
-    updateCondidatStatus(id: number, status: string){
-        return this.condidatApi.updateCondidatStatus(id, status);
-    }
-
-    addNoteToCondidat(id: number, notes: string){
-      return this.condidatApi.addNoteToCondidat(id, notes)
-    }
-    
+  addNoteToCondidat(id: number, notes: string) {
+    return this.condidatApi.addNoteToCondidat(id, notes);
+  }
 }
