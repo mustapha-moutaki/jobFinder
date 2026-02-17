9045753ec4b0f555e34ce56fe5304ac8bf778114
mustapha <mustaphaamoutaki@gmail.com>
2026-02-17 23:36:54 +0000

    style: organze the code

--- src/app/core/services/jobs.service.ts
+++ src/app/core/services/jobs.service.ts
@@ -1,30 +1,24 @@
-import { inject, Injectable } from "@angular/core";
-import { JobApi } from "../../api/jobs.api";
-import { map, Observable, retry } from "rxjs";
-import { Job, PageResponse } from "../models/job.model";
-import { Condidat } from "../models/condidat.model";
+import { inject, Injectable } from '@angular/core';
+import { JobApi } from '../../api/jobs.api';
+import { map, Observable, retry } from 'rxjs';
+import { Job, PageResponse } from '../models/job.model';
+import { Condidat } from '../models/condidat.model';
 
-@Injectable({providedIn: 'root'})
-export class JobService{
-    private readonly jobApi = inject(JobApi); 
-    
-    getAllJobs(page: number = 1, visa:boolean=false, remote:boolean=false ): Observable<PageResponse<Job>> { 
-         return this.jobApi.getAllJobs(page, visa, remote); 
-    }
+@Injectable({ providedIn: 'root' })
+export class JobService {
+  private readonly jobApi = inject(JobApi);
 
-
-     
-    getJobBySlug(slug: string) {
-  return this.jobApi.getAllJobs(0, false, false).pipe(
-    map(response => response.data.find(job => job.slug === slug))
-  );
-
+  getAllJobs(
+    page: number = 1,
+    visa: boolean = false,
+    remote: boolean = false,
+  ): Observable<PageResponse<Job>> {
+    return this.jobApi.getAllJobs(page, visa, remote);
   }
 
-
-
-  
-
-    
+  getJobBySlug(slug: string) {
+    return this.jobApi
+      .getAllJobs(0, false, false)
+      .pipe(map((response) => response.data.find((job) => job.slug === slug)));
+  }
 }
-
